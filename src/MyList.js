0d7c1220a6ff1486f825dc40400e4e9bb76bc73f
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-12 16:54:09 -0400

    prop fixes

--- src/MyList.js
+++ src/MyList.js
@@ -3,24 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import ListItem from './components/ListItem';
 
-// function MyList(props) {
-//   const todoItems = props.theList.map((item, index)=>(
-//     <ListItem key={index} doThis={item}/>
-//   ))
-
- 
 function MyList() {
+  const myListData = ["Schedule Dentist Apt", "Order Cat Food", "Fold Laundry", "Reschedule Dance Class", "Call Mom and Dad", "Finish React Assignment"]; // Replace this with your data
   return (
     <div>
       <header style={{color: "black", textAlign: "left", fontSize: "45px", fontWeight: "bold", textDecoration: "underline"}}>Things to Get Done</header>
-      <div>
-        <ul>
-          <ListItem/>
-        </ul>
-      </div>
+      <ul>
+        {myListData.map((item, index) => (
+          <ListItem key={index} doThis={`${item}`} /> 
+        ))}
+      </ul>
     </div>
   );
 }
-
 
 export default MyList;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,17 +1,10 @@
-import React from 'react'
 
 const ListItem = (props) => {
-  return (
-    <div>
-        <li>Call Mom</li>
-        <li>Schedule Dentist Apt</li>
-        <li>Order Cat Food</li>
-        <li>Fold Laundry</li>
-        <li>Reschedule Dance Class</li>
-        <li>Finish React Assignment</li>
+    return (
+      <li style={{fontSize: "17px"}}>
         {props.doThis}
-    </div>
-  )
-}
-
-export default ListItem
+      </li>
+    );
+  }
+  
+  export default ListItem;
